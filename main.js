d179a08ef9ea90c58856885ac313850352db11a3
Patzi Acosta <patziacosta40@gmail.com>
2026-03-05 18:30:57 -0600

    deletion of html

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
 // https://api.giphy.com/v1/gifs/search?api_key=KquNNPgL1wOABIWN5FsFVOhbUUGfS4mM&q=${keyword}&limit=12
 
+// $(document).ready(()=>{
+
+
+// $("#submit").click(()=>{
+//     let userInput = $("#search").val()
+//     alert(userInpunt)
+//     $.ajax({
+//     url:"https://api.giphy.com/v1/gifs/search?api_key=KquNNPgL1wOABIWN5FsFVOhbUUGfS4mM&q=${keyword}&limit=12"
+// })
+// })    
+// $.ajax({
+//     url:"https://api.giphy.com/v1/gifs/search?api_key=KquNNPgL1wOABIWN5FsFVOhbUUGfS4mM&q=${keyword}&limit=12"
+// })
+
+// })
+
+$.get('https://api.giphy.com/v1/gifs/search?api_key=KquNNPgL1wOABIWN5FsFVOhbUUGfS4mM&q=${keyword}&limit=12').then((data)=>{
+    console.log(data)
+})

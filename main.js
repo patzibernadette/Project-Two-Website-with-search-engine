74d36c64683db9575f2e8f88c90363672bfb801c
Patzi Acosta <patziacosta40@gmail.com>
2026-03-05 20:20:08 -0600

    Changes to HTML with Images

--- main.js
+++ main.js
@@ -1,21 +1,39 @@
+let results = document.getElementById("results");
+let form = document.getElementById("searchForm");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault(); 
+
+    let keyword = document.getElementById("searchInput").values;
+
+    if (keyword === "") {
+        alert("Please enter a search keyword");
+        return;
+    }
+    results.innerHTML = "Loading...";
+    fetch('https://api.giphy.com/v1/gifs/search?api_key=KquNNPgL1wOABIWN5FsFVOhbUUGfS4mM&q=${keyword}&limit=12')
+     .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+
+        results.innerHTML = ""; 
+
+        data.data.forEach(function (gif){
+            let img = document.createElement("img");
+            img.src = gif.images.fixed_height.url;
+            results.appendChild(img);
+        });
+
+    })
+    .catch(function (error) {
+        results.innerHTML = "Something went wrong.";
+        console.log("Error:", error);
+    });
+});
+
+
 // https://api.giphy.com/v1/gifs/search?api_key=KquNNPgL1wOABIWN5FsFVOhbUUGfS4mM&q=${keyword}&limit=12
 
-// $(document).ready(()=>{
 
 
-// $("#submit").click(()=>{
-//     let userInput = $("#search").val()
-//     alert(userInpunt)
-//     $.ajax({
-//     url:"https://api.giphy.com/v1/gifs/search?api_key=KquNNPgL1wOABIWN5FsFVOhbUUGfS4mM&q=${keyword}&limit=12"
-// })
-// })    
-// $.ajax({
-//     url:"https://api.giphy.com/v1/gifs/search?api_key=KquNNPgL1wOABIWN5FsFVOhbUUGfS4mM&q=${keyword}&limit=12"
-// })
-
-// })
-
-$.get('https://api.giphy.com/v1/gifs/search?api_key=KquNNPgL1wOABIWN5FsFVOhbUUGfS4mM&q=${keyword}&limit=12').then((data)=>{
-    console.log(data)
-})
